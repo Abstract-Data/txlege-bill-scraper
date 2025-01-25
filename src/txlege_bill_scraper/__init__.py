342fc874dff646041b11621851de29cad6308cb7
John R. Eakin <48845615+jreakin@users.noreply.github.com>
2025-01-25 02:42:38 -0600

    * Configure the `inject` library to bind the `BuildWebDriver` instance to the required classes
    * Import `inject` and `BuildWebDriver`
    * Define `configure_dependencies` function to bind `BuildWebDriver` instance
    * Call `inject.configure` with `configure_dependencies` function

--- src/txlege_bill_scraper/__init__.py
+++ src/txlege_bill_scraper/__init__.py
@@ -1,2 +1,10 @@
+import inject
+from driver import BuildWebDriver
+
+def configure_dependencies(binder: inject.Binder) -> None:
+    binder.bind(BuildWebDriver, BuildWebDriver())
+
+inject.configure(configure_dependencies)
+
 def main() -> None:
     print("Hello from txlege-bill-scraper!")
